0d708a36a06237f7bfd50cd77fd1b12c321da2a0
Raúl López González <apple@MacBook-Pro-de-Apple.local>
2022-05-30 17:02:01 -0500

    Arreglo de subscriptores usando arreglos V1

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,42 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [number, setNumber] = useState<number | string>(5)
-
-  const changeNumber = () => {
-    setNumber("2")
-  }
+  const [subs, setSubs] = useState([
+    {
+      nick: 'dapelu',
+      subMonths: 3,
+      avatar: 'https://i.pravatar.cc/150?u=dapelu',
+      description: 'Dapelu hace de moderador a veces'
+    },
+    {
+      nick: 'sergio_serrano',
+      subMonths: 7,
+      avatar: 'https://i.pravatar.cc/150?u=sergio_serrano'
+    },
+    {
+      nick: 'incocnitto',
+      subMonths: 10,
+      avatar: 'https://i.pravatar.cc/150?u=incocnitto',
+      description: 'Streamer de vez en cuando'
+    }
+  ])
 
   return (
     <div className="App">
-      {number}
-      <button onClick={changeNumber}>Change Number</button>
+      <h1>midu subs</h1>
+      <ul>
+        {
+          subs.map(sub =>{
+            return(
+                <li key={sub.nick} >
+                  <img src={sub.avatar} alt={`Avatar for ${sub.nick}`} />
+                  <h4>{sub.nick}(<small>{sub.subMonths}</small>)</h4>
+                  <p>{sub.description?.substring(0, 100)}</p>
+                </li>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
